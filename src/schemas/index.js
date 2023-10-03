67ccce3f6bef3f4ba21ff01630c735c695bdf4af
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-03 10:51:06 +0500

    yep validation schemas

---
+++ src/schemas/index.js
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+const SignupSchema = Yup.object().shape({
+    email: Yup.string().matches(emailRegex, 'Invalid email format').email('Invalid email').required('Email Is Required'),
+    password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+      'Password must include at least one uppercase letter, one lowercase letter, and one number'
+    )
+    .max(8, 'Password must be exactly 8 characters long')
+    .required('Password is required'),
+  });
+ export default SignupSchema
